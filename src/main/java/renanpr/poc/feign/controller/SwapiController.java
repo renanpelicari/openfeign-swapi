ac40e9c37cef9811dbcab8929c30014f4aff1282
Renan Peliçari <renanpelicari@gmail.com>
2018-10-24 18:28:16 -0300

    fixed

--- src/main/java/renanpr/poc/feign/controller/SwapiController.java
+++ src/main/java/renanpr/poc/feign/controller/SwapiController.java
@@ -1,6 +1,5 @@
 package renanpr.poc.feign.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
